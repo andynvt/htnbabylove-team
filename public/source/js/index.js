941bac709278df066402ab16797ae10aee6faf79
TauRus <phipham4596@gmail.com>
2018-03-22 10:50:45 +0700

    alert

--- public/source/js/index.js
+++ public/source/js/index.js
@@ -1,32 +1,24 @@
 jQuery(function ($) {
     function fixDiv() {
+        alert("dsah");
         var $cache = $('#getFixed');
         if ($(window).scrollTop() > 260) $cache.css({
-            'position': 'fixed'
-            , 'top': '0'
-            , 'width': '100%'
-            , 'background': 'white'
-            , 'z-index': '999'
-            , 'height': '53px'
+            'position': 'fixed',
+            'top': '0',
+            'width': '100%',
+            'background': 'white',
+            'z-index': '999',
+            'height': '53px'
         });
         else $cache.css({
-            'position': 'relative'
-            , 'top': 'auto'
+            'position': 'relative',
+            'top': 'auto'
         });
     }
     $(window).scroll(fixDiv);
     fixDiv();
 });
-//$(document).ready(function () {
-//    $('.hamburger').click(function () {
-//        if ($(".div-hamburger").find('span.fa').hasClass('fa-bars')) {
-//            $(".div-hamburger").find('span.fa').removeClass('fa-bars').addClass('fa-times');
-//        } else {
-//            $(".div-hamburger").find('span.fa').removeClass('fa-times').addClass('fa-bars');
-//        }
-//
-//    })
-//});
+
 function openNav() {
     document.getElementById("mySidenav").style.width = "70%";
 }
