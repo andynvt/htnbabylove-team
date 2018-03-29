ca5bcac1ef45cdfa28611936ab53d1b89d499602
andynvt <andy.nvt.vn@gmail.com>
2018-03-29 17:16:37 +0700

    dịch thuật

--- resources/views/Admin/master.blade.php
+++ resources/views/Admin/master.blade.php
@@ -117,8 +117,8 @@
                     </div>
                 </div>
                 <div class="modal-footer">
-                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                    <button type="button" class="btn btn-primary">Save changes</button>
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Đóng</button>
+                    <button type="button" class="btn btn-primary">Lưu lại</button>
                 </div>
             </div>
             <!-- /.modal-content -->
