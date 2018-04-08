5ffa9214352b5b06b56785550817aa276f45a5b7
TauRus <phipham4596@gmail.com>
2018-04-08 11:10:33 +0700

    Cập nhật lại chi tiết sản phẩm

--- resources/views/page/chitiet_sanpham.blade.php
+++ resources/views/page/chitiet_sanpham.blade.php
@@ -206,12 +206,19 @@
                 <div class="content-rate-product">
                     <div class="title-rate">
                         <div class="title">
-                            <h3>Đánh giá sản phẩm {{ $sanpham->name }}</h3>
+                            <h3 style="text-transform: initial;">
+                                @if(count($feedback) == 0)
+                                    Không có đánh giá nào
+                                @else
+                                    Có {{ count($feedback) }} đánh giá sản phẩm {{ $sanpham->name }}
+                                @endif
+                            </h3>
                         </div>
                         <div class="go-to-rate">
                             <!-- <a href="#text-comment">Gửi đánh giá của bạn</a> -->
                             <!-- Button to Open the Modal -->
-                            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#feedback"> Gửi đánh giá của bạn </button>
+
+                            <button type="button" class="btn-sendfb" data-toggle="modal" data-target="#feedback"> Gửi đánh giá của bạn </button>
                         </div>
                     </div>
                         @foreach($feedback as $fb) 
@@ -220,11 +227,13 @@
                                     <div class="time-post"> <span>{{$fb->create_at}}</span> </div>
                                     <div class="star-rate">
                                         <div class="start-sum">
-                                            <i class="fa fa-star"></i>
-                                            <i class="fa fa-star"></i>
-                                            <i class="fa fa-star"></i>
-                                            <i class="fa fa-star"></i>
-                                            <i class="fa fa-star"></i>
+                                            <input id="{{ $fb->id_feedback }}" type="hidden" value="{{ $fb->stars }}">
+                                            <div class="khong-duoc-xoa"></div>
+                                            <script>
+                                                for($i=0; $i < {{ $fb->stars }} ; $i++){
+                                                    $('#{{ $fb->id_feedback }}').next('.khong-duoc-xoa').append('<i class="fa fa-star fa-fw"></i>');
+                                                }
+                                            </script>
                                         </div>
                                         <div class="comment">
                                             <p>{{ $fb->review }}</p>
