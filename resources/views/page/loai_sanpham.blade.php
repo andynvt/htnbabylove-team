e3e05d3c50fc1316dd359a710f1bf395e0449a3e
Huỳnh Khắc Duy <anminh113@gmail.com>
2018-04-08 13:34:28 +0700

    sửa tìm kiếm theo loại

--- resources/views/page/loai_sanpham.blade.php
+++ resources/views/page/loai_sanpham.blade.php
@@ -4,9 +4,9 @@
 @extends('master')
 @section('content')
 
-	<div class="container-fluid">
-		<div class="row">
-			<div class="col-lg-3 col-md-12 col-sm-12 col-xs-12  ">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-lg-3 col-md-12 col-sm-12 col-xs-12  ">
             <!-- Tim kiem nhanh san pham -->
             <div id="navbar">
                 <!-- Menu -->
@@ -74,28 +74,29 @@
         </div>
         <div class="col-lg-9 col-md-12">
             <div class="space10">&nbsp;</div>
-            <div class="text-left" style="font-size: 25px; font-weight: 600"> {{$loai_ssp->type_name}} ({{count('$sp_theoloai')}} sản phẩm)</div>
+            <div class="text-left" style="font-size: 25px; font-weight: 600"> {{$loai_ssp->type_name}} ({{count("$sp_theoloai")}} sản phẩm)</div>
             <div class="space15">&nbsp;</div>
             <div class="row">
-            @foreach($sp_theoloai as $sp)
-            	@foreach($id_product as $product)
-            		@if($sp->id_type == $product->id_type)
-            			@foreach($detail_product as $detail)
-            				@if($product->id_product == $detail->id_product)
+            
+                @foreach($id_product as $product)
+                @foreach($sp_theoloai as $sp)
+                    @if($product->id_type ==$sp->id_type )
+                        @foreach($detail_product as $detail)
+                            @if($product->id_product == $detail->id_product)
                 <div class="col-lg-4 col-md-6 col-sm-6 col-xs-6">
                     <div class="single-item">
                         <div class="ribbon-wrapper">
-                        @if($sp->promotion_price != 0)
+                        @if($product->promotion_price != 0)
                             <div class="ribbon1 sale">Sale</div>
-                        @elseif($sp->status == 1)
+                        @elseif($product->status == 1)
                             <div class="ribbon1 new">New</div>
-                        @elseif($sp->status == 2)
+                        @elseif($product->status == 2)
                             <div class="ribbon1 hot">Hot</div>
                         @endif
                         </div>
                         <div class="thumbnail">
                             <div class="containeroverlay">
-                                <a href="#"><img src="source/image/{{ $detail->image }}" class="img-responsive"></a>
+                                <a href="#"><img src="source/image/{{ $detail->image }}" class="img-responsive" width="480px"></a>
                                 <div class="overlay">
                                     <div class="text">Xem chi tiết</div>
                                 </div>
@@ -103,25 +104,28 @@
                             <div class="caption">
                                 <div class="space5">&nbsp;</div>
                                 <b class="text-price">
-                                @if($sp->promotion_price == 0)
-                                    <span class="text-danger ">{{number_format($sp->unit_price)}} đ</span> &nbsp;
+                                @if($product->promotion_price == 0)
+                                    <span class="text-danger ">{{number_format($product->unit_price)}} đ</span> &nbsp;
                                 @else
-                                    <span class="text-danger ">{{number_format($sp->promotion_price)}} đ</span> &nbsp;
-                                    <span class="flash-del">{{number_format($sp->unit_price)}} đ</span>
+                                    <span class="text-danger ">{{number_format($product->promotion_price)}} đ</span> &nbsp;
+                                    <span class="flash-del">{{number_format($product->unit_price)}} đ</span>
                                 @endif
                                 </b>
                                 <div class="space10">&nbsp;</div>
-                                <p class='text-left text-title'><b>{{$sp->name}}</b>&nbsp;</p>
-                                <button type="button" class="btn btn-buy btn-full button" data-toggle="modal" data-target="#1"><span>Mua Ngay </span></button>
+                                <p class='text-left text-title'><b>{{$product->name}}</b>&nbsp;</p>
+                                <button type="button" class="btn btn-buy btn-full button" data-toggle="modal" data-target="#{{$product->id_product}}"><span>Mua Ngay </span></button>
                                 <button type="button" class="btn btn-themvaogio btn-full ">Thêm vào giỏ</button>
                                 <hr>
                             </div>
                         </div>
                     </div>
-                </div>		
-                			@endif
-                		@endforeach
-                   	@endif
+                </div>      
+                               
+                            @endif
+                        @endforeach
+                        @break
+                    @endif
+
                 @endforeach
             @endforeach
             </div>
@@ -135,7 +139,7 @@
 
 
         </div>
-		</div>
+        </div>
         
     </div>
 
