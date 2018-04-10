f198f71584aeaaf975f66c04b078c9ec53de2b53
andynvt <andy.nvt.vn@gmail.com>
2018-04-10 16:30:10 +0700

    fx

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -61,7 +61,7 @@
                                         <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
                                             <div class="carousel-inner" role="listbox">
                                                 <div class="carousel-item active">
-                                                    <a href="#"><img class="d-block img-fluid img-slide" src="source/image/goi.jpg"></a>
+                                                    <a href="#"><img class="d-block img-fluid img-slide" src="source/image/product_1.jpg"></a>
                                                 </div>
                                                 <div class="carousel-item">
                                                     <a href="#"><img class="d-block img-fluid img-slide" src="source/image/goi.jpg"></a>
@@ -70,8 +70,8 @@
                                                     <a href="#"><img class="d-block img-fluid img-slide" src="source/image/goi.jpg"></a>
                                                 </div>
                                             </div>
-                                            <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev"> <span class="carousel-control-prev-icon" aria-hidden="true"></span> <span class="sr-only">Previous</span> </a>
-                                            <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next"> <span class="carousel-control-next-icon" aria-hidden="true"></span> <span class="sr-only">Next</span> </a>
+                                            <a class="carousel-control-prev" type="button" href="#carouselExampleControls" role="button" data-slide="prev"> <span class="carousel-control-prev-icon" aria-hidden="true"></span> <span class="sr-only">Previous</span> </a>
+                                            <a class="carousel-control-next" type="button" href="#carouselExampleControls" role="button" data-slide="next"> <span class="carousel-control-next-icon" aria-hidden="true"></span> <span class="sr-only">Next</span> </a>
                                         </div>
                                     </div>
                                 </div>
