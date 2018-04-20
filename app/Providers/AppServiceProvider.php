65e3a3cf1e9feb9555601ad7c22fd0a0c63a6ef8
VoHoaiPhong <vo.hoaiphongamkg@gmail.com>
2018-04-20 14:59:44 +0700

    Change
    view()->composer('Admin.pageadmin.nav',function($view){
            view()->composer('Admin.pageadmin.adminnav',function($view)

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -8,9 +8,6 @@ use App\ProductType;
 use App\Product;
 use App\ProductDetail;
 use App\Bill;
-use App\ProductImage;
-use App\ProductColor;
-use DB;
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -25,25 +22,6 @@ class AppServiceProvider extends ServiceProvider
         view()->composer('header',function($view){
             $loai_sanpham = ProductType::all();
             $view->with('loai_sanpham',$loai_sanpham);
-        });
-
-        view()->composer('master',function($view){
-            $product_image = ProductImage::all();
-            $view->with('product_image',$product_image);
-        });
-
-        view()->composer('master',function($view){
-            // $product_color = DB::select(DB::raw('SELECT color FROM product_color WHERE id_detail in (SELECT id FROM product_detail WHERE id_product in (SELECT id FROM products ))'));
-
-            $product_color = DB::table('products as sp')
-                        ->Join('product_detail as detail','sp.id','=','detail.id_product')
-                        ->Join('product_color as color','detail.id','=','color.id_detail')
-                        ->select('color.id_detail','color.id','color.color')
-                        ->distinct()
-                        ->get();
-            // dd($product_color);
-
-            $view->with('product_color',$product_color);
         });
 
         view()->composer('master',function($view){
@@ -66,7 +44,6 @@ class AppServiceProvider extends ServiceProvider
             $view->with('detail_product',$detail_product);
         });
 
-
         view()->composer('Admin.pageadmin.admindanhgia', function($view){
             $loai_sp = ProductType::all();
             $view->with('loai_sp',$loai_sp);
@@ -77,7 +54,7 @@ class AppServiceProvider extends ServiceProvider
         //     $view->with('l_sanpham',$l_sanpham);
         // });
 
-        view()->composer('Admin.pageadmin.nav',function($view){
+        view()->composer('Admin.pageadmin.adminnav',function($view){
             $confirm_bill = Bill::where('status', 4)->get();
             $view->with('confirm_bill',$confirm_bill);
         });
