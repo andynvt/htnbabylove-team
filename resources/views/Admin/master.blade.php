ce38848b4962d895de53f689f1fde73ce89012be
TauRus <phipham4596@gmail.com>
2018-04-20 08:29:01 +0700

    Update admin sản phẩm lần 1

--- resources/views/Admin/master.blade.php
+++ resources/views/Admin/master.blade.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8" />
     <link rel="shortcut icon" href="source/image/favicon.png" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+    <base href="{{asset('')}}">
     <title>HOÀNG THỦY NGUYÊN</title>
     <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
     <meta name="viewport" content="width=device-width" />
