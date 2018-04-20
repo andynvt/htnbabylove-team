5ca933a33e555b4d982cd7575620a0ffdca05ea9
VoHoaiPhong <vo.hoaiphongamkg@gmail.com>
2018-04-20 15:07:32 +0700

    @include('Admin.pageadmin.adminnav')

--- resources/views/Admin/pageadmin/adminsuasanpham.blade.php
+++ resources/views/Admin/pageadmin/adminsuasanpham.blade.php
@@ -62,21 +62,7 @@
                 <?php date_default_timezone_set('Asia/Ho_Chi_Minh');echo date('d/m/Y - H:i\p\m'); ?>
             </small> </div>
                     <div class="collapse navbar-collapse">
-                        <ul class="nav navbar-nav navbar-right">
-                            <li class="dropdown">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown"> <i class="fa fa-globe"></i>
-                                    <p>Thông Báo</p> <span class="badge" style="background-color:#FF4066">1</span> </a>
-                            </li>
-                            <li class="dropdown">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown"> <i class="ti-bell"></i>
-                                    <p>Admin</p> <b class="caret"></b> </a>
-                                <ul class="dropdown-menu">
-                                    <li><a href="#"><i class="fa fa-user" aria-hidden="true"></i>Cá Nhân</a></li>
-                                    <li><a href="#"><i class="fa fa-cogs" aria-hidden="true"></i>Cài Đặt</a></li>
-                                    <li><a href="#"><i class="fa fa-sign-out" aria-hidden="true"></i>Đăng Xuất</a></li>
-                                </ul>
-                            </li>
-                        </ul>
+                        @include('Admin.pageadmin.adminnav')
                     </div>
                 </div>
             </nav>
