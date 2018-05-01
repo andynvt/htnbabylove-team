5574bbeebce348cd6aa3b512d9106a6bfc4bc061
ANDY <andy.nvt.vn@gmail.com>
2018-05-01 14:32:13 +0700

    fix lỗi cực kỳ lớn trong header

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -73,7 +73,7 @@
                                         <div style="display: none">{{$i++}}</div>
                                         @endforeach
                                     </div>
-                                    <a class="carousel-control-prev" type="button" href="#carouselExampleControls" role="button" data-slide="prev"> <span class="carousel-control-prev-icon" aria-hidden="false"></span> <span class="sr-only">Previous</span> </a>
+                                    <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev"> <span class="carousel-control-prev-icon" aria-hidden="false"></span> <span class="sr-only">Previous</span> </a>
                                     <a class="carousel-control-next"  href="#carouselExampleControls" role="button" data-slide="next"> <span class="carousel-control-next-icon" aria-hidden="true"></span> <span class="sr-only">Next</span> </a>
                                 </div>
                             </div>
