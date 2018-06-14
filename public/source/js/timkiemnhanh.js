74e04a6234f5f58a1e25911c42997f9e4e06290d
TauRus <phipham4596@gmail.com>
2018-06-14 21:15:28 +0700

    chuyển filter thành kiểu tiền

--- public/source/js/timkiemnhanh.js
+++ public/source/js/timkiemnhanh.js
@@ -3,18 +3,25 @@ $(function () {
         range: true
         , min: 0
         , max: 2500000
-        , step: 100
+        , step: 1000
         , value: [0, 2500000]
-        , slide: function (event, ui) {
-            $("#app_min_price").html("$" + ui.value[0]);
-            $("#app_max_price").html("$" + ui.values[1]);
-        }
-        , stop: function (event, ui) {
-            $("#app_min_price").html("$" + ui.value[0]);
-            $("#app_max_price").html("$" + ui.value[1]);
-        }
+        // , slide: function (event, ui) {
+        //     $("#app_min_price").html("$" + ui.value[0]);
+        //     $("#app_max_price").html("$" + ui.value[1]);
+        // }
+        // , stop: function (event, ui) {
+        //     $("#app_min_price").html("$" + ui.value[0]);
+        //     $("#app_max_price").html("$" + ui.value[1]);
+        // }
     , }).on('slide', function () {
-        $('#outputmin').html(this.value);
-        $('#outputmax').html(this.value);
+        // $('#outputmin').html(this.value);
+        $('#outputmin').html(formatCurrency(this.value));
+        // $('#outputmax').html(this.value);
     }).trigger('slide');
 });
+
+function formatCurrency(number){
+    var n = number.split('').reverse().join("");
+    var n2 = n.replace(/\d\d\d(?!$)/g, "$&,");    
+    return  n2.split('').reverse().join('') + 'VNĐ';
+}
